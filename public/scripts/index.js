298d8ed5a6903c4ad4c6ea21bd174ccd64a2f0cf
denys mohylin <dmohylin@bizico.com>
2020-04-01 18:51:53 +0300

    [feat] new subproject

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -8,135 +8,164 @@ const { RTCPeerConnection, RTCSessionDescription } = window;
 const peerConnection = new RTCPeerConnection();
 
 function unselectUsersFromList() {
-    const alreadySelectedUser = document.querySelectorAll(
-        ".active-user.active-user--selected"
-    );
+  const alreadySelectedUser = document.querySelectorAll(
+    '.active-user.active-user--selected'
+  );
 
-    alreadySelectedUser.forEach(el => {
-        el.setAttribute("class", "active-user");
-    });
+  alreadySelectedUser.forEach((el) => {
+    el.setAttribute('class', 'active-user');
+  });
 }
 
 function createUserItemContainer(socketId) {
-    const userContainerEl = document.createElement("div");
+  const userContainerEl = document.createElement('div');
 
-    const usernameEl = document.createElement("p");
+  const usernameEl = document.createElement('p');
 
-    userContainerEl.setAttribute("class", "active-user");
-    userContainerEl.setAttribute("id", socketId);
-    usernameEl.setAttribute("class", "username");
-    usernameEl.innerHTML = `Socket: ${socketId}`;
+  userContainerEl.setAttribute('class', 'active-user');
+  userContainerEl.setAttribute('id', socketId);
+  usernameEl.setAttribute('class', 'username');
+  usernameEl.innerHTML = `Socket: ${socketId}`;
 
-    userContainerEl.appendChild(usernameEl);
+  userContainerEl.appendChild(usernameEl);
 
-    userContainerEl.addEventListener("click", () => {
-        unselectUsersFromList();
-        userContainerEl.setAttribute("class", "active-user active-user--selected");
-        const talkingWithInfo = document.getElementById("talking-with-info");
-        talkingWithInfo.innerHTML = `Talking with: "Socket: ${socketId}"`;
-        callUser(socketId);
-    });
+  userContainerEl.addEventListener('click', () => {
+    unselectUsersFromList();
+    userContainerEl.setAttribute('class', 'active-user active-user--selected');
+    const talkingWithInfo = document.getElementById('talking-with-info');
+    talkingWithInfo.innerHTML = `Talking with: "Socket: ${socketId}"`;
+    callUser(socketId);
+  });
 
-    return userContainerEl;
+  return userContainerEl;
 }
 
 async function callUser(socketId) {
-    const offer = await peerConnection.createOffer();
+  const offer = await peerConnection.createOffer();
+  try {
     await peerConnection.setLocalDescription(new RTCSessionDescription(offer));
-
-    socket.emit("call-user", {
-        offer,
-        to: socketId
-    });
+    // console.log(offer);
+  } catch (error) {
+    console.log(error);
+  }
+  socket.emit('call-user', {
+    offer,
+    to: socketId
+  });
 }
 
 function updateUserList(socketIds) {
-    const activeUserContainer = document.getElementById("active-user-container");
+  const activeUserContainer = document.getElementById('active-user-container');
 
-    socketIds.forEach(socketId => {
-        const alreadyExistingUser = document.getElementById(socketId);
-        if (!alreadyExistingUser) {
-            const userContainerEl = createUserItemContainer(socketId);
+  socketIds.forEach((socketId) => {
+    const alreadyExistingUser = document.getElementById(socketId);
+    if (!alreadyExistingUser) {
+      const userContainerEl = createUserItemContainer(socketId);
 
-            activeUserContainer.appendChild(userContainerEl);
-        }
-    });
+      activeUserContainer.appendChild(userContainerEl);
+    }
+  });
 }
 
-const socket = io.connect("localhost:5000");
+const socket = io.connect('localhost:5000');
 
-socket.on("update-user-list", ({ users }) => {
-    updateUserList(users);
+socket.on('update-user-list', ({ users }) => {
+  updateUserList(users);
 });
 
-socket.on("remove-user", ({ socketId }) => {
-    const elToRemove = document.getElementById(socketId);
+socket.on('remove-user', ({ socketId }) => {
+  const elToRemove = document.getElementById(socketId);
 
-    if (elToRemove) {
-        elToRemove.remove();
-    }
+  if (elToRemove) {
+    elToRemove.remove();
+  }
 });
 
-socket.on("call-made", async data => {
-    if (getCalled) {
-        const confirmed = confirm(
-            `User "Socket: ${data.socket}" wants to call you. Do accept this call?`
-        );
-
-        if (!confirmed) {
-            socket.emit("reject-call", {
-                from: data.socket
-            });
-
-            return;
-        }
-    }
-
-    await peerConnection.setRemoteDescription(
-        new RTCSessionDescription(data.offer)
+socket.on('call-made', async (data) => {
+  if (getCalled) {
+    const confirmed = confirm(
+      `User "Socket: ${data.socket}" wants to call you. Do accept this call?`
     );
+
+    if (!confirmed) {
+      socket.emit('reject-call', {
+        from: data.socket
+      });
+
+      return;
+    }
+  }
+  try {
+    await peerConnection.setRemoteDescription(
+      new RTCSessionDescription(data.offer)
+    );
+
     const answer = await peerConnection.createAnswer();
+
     await peerConnection.setLocalDescription(new RTCSessionDescription(answer));
+  } catch (error) {
+    console.log('peerConnection', error);
+  }
 
-    socket.emit("make-answer", {
-        answer,
-        to: data.socket
-    });
-    getCalled = true;
+  socket.emit('make-answer', {
+    answer,
+    to: data.socket
+  });
+  getCalled = true;
 });
 
-socket.on("answer-made", async data => {
+socket.on('answer-made', async (data) => {
+  try {
     await peerConnection.setRemoteDescription(
-        new RTCSessionDescription(data.answer)
+      new RTCSessionDescription(data.answer)
     );
+  } catch (error) {
+    console.log(error);
+  }
 
-    if (!isAlreadyCalling) {
-        callUser(data.socket);
-        isAlreadyCalling = true;
-    }
+  if (!isAlreadyCalling) {
+    callUser(data.socket);
+    isAlreadyCalling = true;
+  }
 });
 
-socket.on("call-rejected", data => {
-    alert(`User: "Socket: ${data.socket}" rejected your call.`);
-    unselectUsersFromList();
+socket.on('call-rejected', (data) => {
+  alert(`User: "Socket: ${data.socket}" rejected your call.`);
+  unselectUsersFromList();
 });
 
-peerConnection.ontrack = function ({ streams: [stream] }) {
-    const remoteVideo = document.getElementById("remote-video");
-    if (remoteVideo) {
-        remoteVideo.srcObject = stream;
-    }
+peerConnection.ontrack = function({ streams: [stream, ...rest] }) {
+  // const remoteVideo = document.getElementById('remote-video');
+  console.log(rest);
+
+  const newVideoFrame = document.createElement('video');
+  newVideoFrame.setAttribute('class', 'remote-video');
+  newVideoFrame.setAttribute('autoplay', '');
+  const videoContainer = document.querySelector('.video-container');
+  videoContainer.appendChild(newVideoFrame);
+  newVideoFrame.srcObject = stream;
+  // <video autoplay class="remote-video" id="remote-video"></video>
+  // if (remoteVideo) {
+  //   remoteVideo.srcObject = stream;
+  //   remoteVideo.play();
+
+  //   console.log(remoteVideo);
+  // }
 };
 
-navigator.mediaDevices.getUserMedia({ video: true, audio: true })
-    .then((stream) => {
-        const localVideo = document.getElementById("local-video");
-        if (localVideo) {
-            localVideo.srcObject = stream;
-        }
+navigator.mediaDevices
+  .getUserMedia({ video: true, audio: true })
+  .then((stream) => {
+    const localVideo = document.getElementById('local-video');
 
-        stream.getTracks().forEach(track => peerConnection.addTrack(track, stream));
-    })
-    .catch((error) => {
-        console.warn(error.message);
-    });
+    if (localVideo) {
+      localVideo.srcObject = stream;
+    }
+
+    stream
+      .getTracks()
+      .forEach((track) => peerConnection.addTrack(track, stream));
+  })
+  .catch((error) => {
+    console.warn(error.message);
+  });
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
-import express, { Application } from "express";
-import socketIO, { Server as SocketIOServer } from "socket.io";
-import { createServer, Server as HTTPServer } from "http";
-import path from "path";
+import express, { Application } from 'express';
+import socketIO, { Server as SocketIOServer } from 'socket.io';
+import { createServer, Server as HTTPServer } from 'http';
+import path from 'path';
 
 export class Server {
   private httpServer: HTTPServer;
@@ -27,60 +27,62 @@ export class Server {
   }
 
   private configureApp(): void {
-    this.app.use(express.static(path.join(__dirname, "../public")));
+    this.app.use(express.static(path.join(__dirname, '../public')));
   }
 
   private configureRoutes(): void {
-    this.app.get("/", (req, res) => {
-      res.sendFile("index.html");
+    this.app.get('/', (req, res) => {
+      res.sendFile('index.html');
     });
   }
 
   private handleSocketConnection(): void {
-    this.io.on("connection", socket => {
+    this.io.on('connection', (socket) => {
+      console.log('socket connected');
       const existingSocket = this.activeSockets.find(
-        existingSocket => existingSocket === socket.id
+        (existingSocket) => existingSocket === socket.id
       );
 
       if (!existingSocket) {
         this.activeSockets.push(socket.id);
 
-        socket.emit("update-user-list", {
+        socket.emit('update-user-list', {
           users: this.activeSockets.filter(
-            existingSocket => existingSocket !== socket.id
+            (existingSocket) => existingSocket !== socket.id
           )
         });
 
-        socket.broadcast.emit("update-user-list", {
+        socket.broadcast.emit('update-user-list', {
           users: [socket.id]
         });
       }
 
-      socket.on("call-user", (data: any) => {
-        socket.to(data.to).emit("call-made", {
+      socket.on('call-user', (data: any) => {
+        // console.log('data', data);
+        socket.to(data.to).emit('call-made', {
           offer: data.offer,
           socket: socket.id
         });
       });
 
-      socket.on("make-answer", data => {
-        socket.to(data.to).emit("answer-made", {
+      socket.on('make-answer', (data) => {
+        socket.to(data.to).emit('answer-made', {
           socket: socket.id,
           answer: data.answer
         });
       });
 
-      socket.on("reject-call", data => {
-        socket.to(data.from).emit("call-rejected", {
+      socket.on('reject-call', (data) => {
+        socket.to(data.from).emit('call-rejected', {
           socket: socket.id
         });
       });
 
-      socket.on("disconnect", () => {
+      socket.on('disconnect', () => {
         this.activeSockets = this.activeSockets.filter(
-          existingSocket => existingSocket !== socket.id
+          (existingSocket) => existingSocket !== socket.id
         );
-        socket.broadcast.emit("remove-user", {
+        socket.broadcast.emit('remove-user', {
           socketId: socket.id
         });
       });
